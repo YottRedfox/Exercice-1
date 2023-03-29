f2d6eab9bce592bc9da6af19738d8e80221d7b80
game1 <game1@DESKTOP-R47CRH1>
2023-03-29 18:29:50 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prenom, nom;
+            int age;
+            prenom = "Eliott";
+            nom = "CHERKI";
+            age = 18;
+
+            Console.WriteLine("Bonjour" + prenom + " " + nom + ", vous avez" + age + " ans."); ;
+            Console.ReadLine();
+        }
+    }
+}
